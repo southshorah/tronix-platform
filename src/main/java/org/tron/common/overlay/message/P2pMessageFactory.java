f0528237739541353ae14edbc88b4339603ca0cd
wubin01 <wb_bupt@163.com>
2018-03-23 17:36:04 +0800

    afd

--- src/main/java/org/tron/common/overlay/message/P2pMessageFactory.java
+++ src/main/java/org/tron/common/overlay/message/P2pMessageFactory.java
@@ -24,6 +24,9 @@ import org.tron.core.net.message.MessageTypes;
 public class P2pMessageFactory {
 
   public static P2pMessage create(byte[] data) {
+    if (data == null || data.length == 0){
+      return;
+    }
     byte type = data[0];
     byte[] rawData = ArrayUtils.subarray(data, 1, data.length);
     MessageTypes messageType = MessageTypes.fromByte(type);
